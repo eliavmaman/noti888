397d905938dd4b8c23bfac78ed5fa69f883eded3
eliav <eliavmaman2@gmail.com>
2017-07-29 21:19:02 +0300

    fdsdfd

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -143,7 +143,7 @@ exports.createScan = function (req, res) {
         } else {
             console.log('XXXXXXXXX');
             console.log(global.io.emit);
-            global.socket.emit('scanMessage', scan);
+            global.io.emit('scanMessage', scan);
             res.json(scan);
         }
     });
